dc745ab3aa0b5500a828531dda2adb83447b2b34
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-02-10 14:56:26 +0100

    fix error

--- events/handle_loops.c
+++ events/handle_loops.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 19:27:07 by hsorel            #+#    #+#             */
-/*   Updated: 2025/02/10 14:11:24 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:55:41 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	movement_loop(t_data *data)
 	if (data->player->moving_forward == 1)
 		move(data, 'w', data->player->speed);
 	if (data->player->moving_back == 1)
-		move(data, 's', data->player->speed);
+		move(data, 's', -data->player->speed);
 	if (data->player->moving_left == 1)
 		move(data, 'a', data->player->speed);
 	if (data->player->moving_right == 1)
