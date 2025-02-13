146cccecf06f447a5ae17dabccb2d5323c4987eb
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-02-13 13:54:28 +0100

    remove comments

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 17:46:53 by haroldsorel       #+#    #+#             */
-/*   Updated: 2025/02/10 14:10:36 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:54:00 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@ int	minilibx_init(t_data *data)
 		|| init_ea_text(data, data->map) == -1)
 		return (-1);
 	init_hooks(data);
-	// mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.img_ptr, 0, 0);
 	return (0);
 }
 
